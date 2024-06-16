b6dfa170ab64d1ee25cb8c5e565a12c1c115dfce
Horacio <ramirh4@unlv.nevada.edu>
2024-06-16 11:10:18 -0700

    temp print statement

--- time.c
+++ time.c
@@ -2,8 +2,9 @@
 
 int main(int argc, char* argv[]) {
 
-    int retval = fork();
-    printf("Created child process\n");
+    //int retval = fork();
+    //printf("Created child process\n");
+    printf("Hello! This is a test!\n");
 
     return 0;
 }

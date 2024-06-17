e1e254cc552397047937ac5badab10622cb188b2
Horacio <ramirh4@unlv.nevada.edu>
2024-06-16 19:53:44 -0700

    Added print statement to tmtest.c

--- user/tmtest.c
+++ user/tmtest.c
@@ -5,15 +5,10 @@
 int main() {
 
     int x = timeins();
-    printf("Num: %d\n", x);
-
     sleep(1000);
     int y = timeins();
-    printf("New num: %d\n", y);
 
-    int difference = y - x;
-
-    printf("Time in seconds: %d\n", difference);
+    printf("Time in seconds: %d\n", (y - x));
 
 
     return 0;

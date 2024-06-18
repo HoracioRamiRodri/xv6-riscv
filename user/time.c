6bc1cfe7e32c39df1cd0f97106807c609d06e357
Horacio <ramirh4@unlv.nevada.edu>
2024-06-17 21:19:53 -0700

    Added explanatory comments to time.c

--- user/time.c
+++ user/time.c
@@ -4,21 +4,22 @@
 
 int main(int argc, char* argv[]) {
 
-    int time = uptime();
+    int time = uptime(); // Get the current time
 
-    int ret_val = fork();
+    int ret_val = fork(); // Create child process
 
-    if (ret_val < 0) {
+    if (ret_val < 0) { // Check if child process was created successfully
         printf("Error: Child process couldn't be created\n");
-        return 1;
+        return 1; // If unsuccessful, print error message then return 1
     } 
-    else if (ret_val == 0) {
+    else if (ret_val == 0) { // If successful, then pass command line args to exec
         exec(argv[1], &argv[2]);
     }
-    else {
-        wait(0);
-        int newTime = uptime();
-        printf("Real-time in ticks: %d\n", (newTime - time));
+    else { 
+        wait(0); // Wait for child process to finish
+        int newTime = uptime(); // Get new uptime
+        printf("Real-time in ticks: %d\n", (newTime - time)); // Find the difference
+                                                              // and return result in ticks.
     }    
 
     return 0;
